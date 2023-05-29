cd4780c2ea8d4cd49b47e74a340323510246a12c
AReeme <areeme@student.neumont.edu>
2023-05-29 17:40:32 -0600

    UI/Win Screen/Lose Screen/GameManager Created

---
+++ SpiderFighterMobile2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject titleUI;
+    [SerializeField] GameObject gameWinUI;
+    [SerializeField] GameObject gameLoseUI;
+    [SerializeField] GameObject controlsUI;
+    [SerializeField] TextMeshProUGUI timerText;
+    //[SerializeField] AudioSource gameMusic;
+    [SerializeField] TextMeshProUGUI scoreUI;
+    [SerializeField] TextMeshProUGUI totalTimeWinText;
+    [SerializeField] TextMeshProUGUI totalTimeLoseText;
+
+    public enum State
+    {
+        TITLE,
+        START_GAME,
+        PLAY_GAME,
+        GAME_WIN,
+        GAME_LOSE
+    }
+
+    public State state = State.TITLE;
+    float stateTimer = 0;
+    public float timer = 0;
+    float initialTime = 0;
+
+    private void Start()
+    {
+        timer = 0;
+        timer += initialTime;
+    }
+
+    public void Update()
+    {
+        switch (state)
+        {
+            case State.TITLE:
+                titleUI.SetActive(true);
+                //gameMusic.Stop();
+                break;
+            case State.START_GAME:
+                titleUI.SetActive(false);
+                //gameMusic.Play();
+                state = State.PLAY_GAME;
+                break;
+            case State.PLAY_GAME:
+                timer += Time.deltaTime;
+                timerText.SetText("Time: " + timer.ToString("0.00"));
+                break;
+            case State.GAME_WIN:
+                timer = 0;
+                stateTimer -= Time.deltaTime;
+                if (stateTimer <= 0)
+                {
+                    gameWinUI.SetActive(false);
+                    state = State.TITLE;
+                }
+                break;
+            case State.GAME_LOSE:
+                timer = 0;
+                stateTimer -= Time.deltaTime;
+                if (stateTimer <= 0)
+                {
+                    gameLoseUI.SetActive(false);
+                    state = State.TITLE;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void SetWin()
+    {
+        gameWinUI.SetActive(true);
+        //gameMusic.Stop();
+        state = State.GAME_WIN;
+        stateTimer = 3;
+
+        // Show total time in the win UI
+        totalTimeWinText.SetText("Total Time: " + timer.ToString("0.00"));
+    }
+
+    public void SetLose()
+    {
+        gameLoseUI.SetActive(true);
+        //gameMusic.Stop();
+        state = State.GAME_LOSE;
+        stateTimer = 5;
+
+        // Show total time in the Lose UI
+        totalTimeLoseText.SetText("Total Time: " + timer.ToString("0.00"));
+    }
+
+    public void StartGame()
+    {
+        state = State.START_GAME;
+    }
+}
